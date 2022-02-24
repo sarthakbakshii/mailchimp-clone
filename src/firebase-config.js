3c86b7642da6a155e82afe4a5ee65711da79acd1
sarthakbakshii <sarthakbakshi14@gmail.com>
2022-02-24 12:12:51 +0530

    sarthak 4

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -16,5 +16,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore();
+export const db = getFirestore(app);
 const analytics = getAnalytics(app);
