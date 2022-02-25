705af9569f03a26f803b8d84cfa4f8fb7b034a99
Aditya Pathak <91533065+adityapathak-auto@users.noreply.github.com>
2022-02-25 10:59:43 +0530

    false

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import {
   Route,
 } from "react-router-dom";
 import { Home} from './Components/Home'
-import { SearchPage} from './Components/search/SearchPage'
+import { SearchPage} from './Components/search/SearchPage';
+import {Navbar} from "./Components/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
-        <TestNavbar/>
+        <Navbar/>
         <Routes>
             <Route path={"/"} element={ <Home/> } />
             <Route path={"/search"} element={ <SearchPage/> } />
