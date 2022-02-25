2d341322a3cb31bca7c452cde8a8b0948ee2b80c
Aditya Pathak <manhit06@gmail.com>
2022-02-25 10:08:53 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
