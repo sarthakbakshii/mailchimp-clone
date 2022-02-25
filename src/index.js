408aa1794b024910477a6087d46040b16e8c0c77
Aditya Pathak <91533065+adityapathak-auto@users.noreply.github.com>
2022-02-25 11:42:56 +0530

    revert

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
